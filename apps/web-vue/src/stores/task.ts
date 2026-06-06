bdb0e4cfe4bf67be2c5c44faa1051d89d5cecb1d
tyrion <1048919271@qq.com>
2026-06-06 10:12:53 +0800

    fix: align web-vue default rule base

--- apps/web-vue/src/stores/task.ts
+++ apps/web-vue/src/stores/task.ts
@@ -25,7 +25,7 @@ export const useTaskStore = defineStore('task', () => {
   const uploadPct = ref(0)
   const detectStage = ref('FORMAT_CHECK')
   const detectPct = ref(35)
-  const ruleId = ref('pku-ug')
+  const ruleId = ref('generic')
 
   const fileSizeText = computed(() => formatSize(fileSize.value))
   const fileTypeLabel = computed(() =>
